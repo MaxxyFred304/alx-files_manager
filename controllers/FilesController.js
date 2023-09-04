9a140f05447a6b0c5542aba775cb56c66dc80922
root <maxfred304@gmail.com>
2023-09-04 12:53:26 -0700

    File data

--- controllers/FilesController.js
+++ controllers/FilesController.js
@@ -1,9 +1,11 @@
 const dbClient = require('../utils/db');
 const redisClient = require('../utils/redis');
 const { ObjectId } = require('mongodb');
+const fs = require('fs');
+const mime = require('mime-types');
 
 class FilesController {
-  static async putPublish(req, res) {
+  static async getFile(req, res) {
     const { token } = req.headers;
     const { id } = req.params;
 
@@ -22,59 +24,40 @@ class FilesController {
     // Convert ID to ObjectId for MongoDB query
     const fileId = ObjectId(id);
 
-    // Find and update the file document
-    const updatedFile = await dbClient
+    // Find the file document
+    const file = await dbClient
       .client
       .db()
       .collection('files')
-      .findOneAndUpdate(
-        { _id: fileId, userId },
-        { $set: { isPublic: true } },
-        { returnOriginal: false }
-      );
+      .findOne({ _id: fileId });
 
-    if (!updatedFile.value) {
+    // Check if the file document exists
+    if (!file) {
       return res.status(404).json({ error: 'Not found' });
     }
 
-    return res.status(200).json(updatedFile.value);
-  }
-
-  static async putUnpublish(req, res) {
-    const { token } = req.headers;
-    const { id } = req.params;
-
-    // Check if the token is provided
-    if (!token) {
-      return res.status(401).json({ error: 'Unauthorized' });
-    }
-
-    // Retrieve the user based on the token
-    const userId = await redisClient.client.get(`auth_${token}`);
-
-    if (!userId) {
-      return res.status(401).json({ error: 'Unauthorized' });
-    }
-
-    // Convert ID to ObjectId for MongoDB query
-    const fileId = ObjectId(id);
-
-    // Find and update the file document
-    const updatedFile = await dbClient
-      .client
-      .db()
-      .collection('files')
-      .findOneAndUpdate(
-        { _id: fileId, userId },
-        { $set: { isPublic: false } },
-        { returnOriginal: false }
-      );
-
-    if (!updatedFile.value) {
+    // Check if the file is public or the user is the owner
+    if (!file.isPublic && file.userId !== userId) {
       return res.status(404).json({ error: 'Not found' });
     }
 
-    return res.status(200).json(updatedFile.value);
+    // Check if the file is a folder
+    if (file.type === 'folder') {
+      return res.status(400).json({ error: "A folder doesn't have content" });
+    }
+
+    // Check if the file exists locally
+    if (!fs.existsSync(file.localPath)) {
+      return res.status(404).json({ error: 'Not found' });
+    }
+
+    // Determine the MIME type based on the file name
+    const mimeType = mime.lookup(file.name);
+
+    // Read and send the file content with the appropriate MIME type
+    const fileContent = fs.readFileSync(file.localPath);
+    res.setHeader('Content-Type', mimeType);
+    res.send(fileContent);
   }
 }
 
